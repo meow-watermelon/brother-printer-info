ac69ff62e37e777b8dc477f7e7c5035fb9237f21
Hui Li <herdingcat@yahoo.com>
2021-11-22 21:52:57 -0800

    add 30 seconds timeout
    Signed-off-by: Hui Li <herdingcat@yahoo.com>

--- brother-printer-info.py
+++ brother-printer-info.py
@@ -17,27 +17,31 @@ if __name__ == '__main__':
     parser.add_argument('--printer', type=str, required=True, help='Printer IP or Hostname')
     args = parser.parse_args()
 
-    resp = requests.get('http://%s/etc/mnt_info.csv' %(args.printer))
-
-    if resp.status_code == 200:
-        with io.StringIO(resp.text, newline='') as resp_stream:
-            csv_data = csv.reader(resp_stream)
-
-            for index, item in enumerate(csv_data):
-                if index == 0:
-                    printer_info_fields = item
-                else:
-                    printer_info_data = item
-
-        # remove the last empty record
-        if printer_info_fields[-1] == "" and printer_info_data[-1] == "":
-            printer_info_fields.pop(-1)
-            printer_info_data.pop(-1)
-
-        printer_info = dict(zip(printer_info_fields, printer_info_data))
-
-        for k,v in printer_info.items():
-            print('%s: %s' %(k, v))
-    else:
-        print('ERROR: Failed to acquire printer %s maintenance information: RC: %d - %s' %(args.printer, resp.status_code, resp.reason))
+    try:
+        resp = requests.get('http://%s/etc/mnt_info.csv' %(args.printer), timeout=30)
+    except:
+        print('ERROR: Request timeout on printer %s 30 seconds.' %(args.printer))
         sys.exit(2)
+    else:
+        if resp.status_code == 200:
+            with io.StringIO(resp.text, newline='') as resp_stream:
+                csv_data = csv.reader(resp_stream)
+
+                for index, item in enumerate(csv_data):
+                    if index == 0:
+                        printer_info_fields = item
+                    else:
+                        printer_info_data = item
+
+            # remove the last empty record
+            if printer_info_fields[-1] == "" and printer_info_data[-1] == "":
+                printer_info_fields.pop(-1)
+                printer_info_data.pop(-1)
+
+            printer_info = dict(zip(printer_info_fields, printer_info_data))
+
+            for k,v in printer_info.items():
+                print('%s: %s' %(k, v))
+        else:
+            print('ERROR: Failed to acquire printer %s maintenance information: RC: %d - %s' %(args.printer, resp.status_code, resp.reason))
+            sys.exit(3)
